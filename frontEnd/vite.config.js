aaa6ae8e1d075c3ed56a6db8becd9b7cdacdf200
vishalkrsoni <vishal.sony1@gmail.com>
2023-03-19 20:51:55 +0530

    dummy data added

--- frontEnd/vite.config.js
+++ frontEnd/vite.config.js
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  //   server: {
+  //     fs: {
+  //       allow: ["./src/assets"],
+  //     },
+  //   },
+});
+
+
+// export default defineConfig({
+//   plugins: [reactRefresh()],
+//   build: {
+//     assetsInlineLimit: 0,
+//   },
+//   server: {
+//     fs: {
+//       allow: ["./src/assets"],
+//     },
+//   },
+// });
